38a674ab5db2e353a3795adb0151c73a0c24d416
rafimen <111264625+rafimen@users.noreply.github.com>
2023-07-25 11:51:59 +0700

    Add files via upload

---
+++ alok.php
@@ -0,0 +1,13 @@
+<?php
+class rumah{
+    public $warna="merah";
+    public $pintu=2;
+    public $jendela=4;
+    public $genteng=20;
+    public $lantai="mamer";
+    public $pagar="besi";
+
+$rumah1 = new rumah();
+echo $rumah1->warna;
+}
+?>
